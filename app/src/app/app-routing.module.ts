b1a8ed3dd5200f96e42c7bb66fd21b0d1f13e4b5
alek98 <alek@spectra-alarm.com>
2021-03-20 11:01:56 +0100

    added login and signup routing

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './components/home-page/home-page.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
 
 const routes: Routes = [
   {
     path: '',
     component: HomePageComponent,
     children: [
+      {
+        path: 'signup',
+        component: SignupComponent  
+      },
+      {
+        path: 'login',
+        component: LoginComponent
+      }
       
     ]
   }
--- app/src/app/components/material.module.ts
+++ app/src/app/components/material.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from "@angular/core";
 import { MatToolbarModule } from "@angular/material/toolbar";
 import { HomePageComponent } from './home-page/home-page.component';
 import { SignupComponent } from './signup/signup.component';
-
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import {MatButtonModule} from '@angular/material/button';
+import {RouterModule} from '@angular/router';
+import { LoginComponent } from './login/login.component'
 @NgModule({
   imports: [
-    MatToolbarModule
+    MatToolbarModule,
+    MatButtonModule,
+    RouterModule,
   ],
-  declarations: [HomePageComponent, SignupComponent]
+  declarations: [HomePageComponent, SignupComponent, ToolbarComponent, LoginComponent]
 })
 export class MaterialModule {}
