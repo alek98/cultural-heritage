c5e8dde8def815cc5b382649e2b5438778631e66
alek98 <alek@spectra-alarm.com>
2021-03-20 09:35:42 +0100

    initial home page component

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './components/home-page/home-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomePageComponent,
+    children: [
+      
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- app/src/app/components/material.module.ts
+++ app/src/app/components/material.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from "@angular/core";
 import { MatToolbarModule } from "@angular/material/toolbar";
+import { HomePageComponent } from './home-page/home-page.component';
+import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
-  exports: [
-    MatToolbarModule,
-  ]
+  imports: [
+    MatToolbarModule
+  ],
+  declarations: [HomePageComponent, SignupComponent]
 })
 export class MaterialModule {}
