2a11ddfb7cd34ac1f381adc7d4c3d6b5c88f3932
alek98 <alek@spectra-alarm.com>
2021-03-23 11:45:09 +0100

    added atuh trigger function on new user signup

--- app/src/app/services/auth.service.ts
+++ app/src/app/services/auth.service.ts
@@ -1,11 +1,8 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth'
 import firebase from 'firebase/app';
-import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/firestore';
+import { AngularFirestore } from '@angular/fire/firestore';
 
-
-import { Observable, of } from 'rxjs';
-import { switchMap } from 'rxjs/operators'
 import { User } from '../models/user.model';
 import { Router } from '@angular/router';
 
@@ -17,17 +14,17 @@ export class AuthService {
   user: User;
 
   constructor(
-    private auth: AngularFireAuth, 
-    private firestore: AngularFirestore, 
+    private auth: AngularFireAuth,
+    private firestore: AngularFirestore,
     private router: Router,
   ) {
-    
-    this.auth.onAuthStateChanged(async user =>{
-      if(user){
+
+    this.auth.onAuthStateChanged(async user => {
+      if (user) {
         let docRef = await this.firestore.doc<User>(`users/${user.uid}`).get().toPromise();
         this.user = docRef.data();
       }
-      else{
+      else {
         this.user = null;
       }
     })
@@ -35,27 +32,11 @@ export class AuthService {
 
   async googleLogin() {
     const provider = new firebase.auth.GoogleAuthProvider()
-    const credential = await this.auth.signInWithPopup(provider);
-    return this.updateUserData(credential.user);
+    return this.auth.signInWithPopup(provider);
   }
-  
+
   logout() {
     this.auth.signOut();
     return this.router.navigate(['/']);
-  }
-
-  updateUserData(user) {
-    // set user data to firestore
-    const userRef: AngularFirestoreDocument<User> = this.firestore.doc(`users/${user.uid}`);
-
-    const userData: User = {
-      uid: user.uid,
-      email: user.email,
-      displayName: user.displayName,
-      photoURL: user.photoURL,
-      role: 'user',
-    };
-
-    return userRef.set(userData, { merge: true });
   }
 }
