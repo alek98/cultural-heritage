cbf4427dcfde2cb68316aa334513314b1eb0f32a
alek98 <alek@spectra-alarm.com>
2021-03-30 09:53:32 +0200

    importing & exporting data from firebase to testdata folder

--- scripts/angular.sh
+++ scripts/angular.sh
@@ -1,4 +1,6 @@
 #! /bin/bash
-echo Starting Angular
+cBlue='\033[01;34m'
+cNone='\033[00m'
+echo "${cBlue}Starting Angular${cNone}"
 cd ../app
 ng serve -o
--- scripts/firebase.sh
+++ scripts/firebase.sh
@@ -1,4 +1,6 @@
 #! /bin/bash
-echo Starting Firebase and Importing Data
+cBlue='\033[01;34m'
+cNone='\033[00m'
+echo "${cBlue}Start Firebase, Import Data and Export Data 🔥 ${cNone}"
 cd ../app
-firebase emulators:start 
+firebase emulators:start --export-on-exit=../testdata --import=../testdata
