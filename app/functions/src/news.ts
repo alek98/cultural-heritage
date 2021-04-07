a32ab0ef55ce7d3c2dda138d0a33fcfd0ff887f9
alek98 <alek@spectra-alarm.com>
2021-04-07 11:26:52 +0200

    check permissions export

--- app/functions/src/news.ts
+++ app/functions/src/news.ts
@@ -1,8 +1,10 @@
 import * as functions from 'firebase-functions'
 import * as admin from 'firebase-admin'
 import { News } from './models/news.model'
+import { checkPermissions } from './permissions'
 
 export const addNews = functions.https.onCall(async (news: News, context) => {
+  await checkPermissions(context);
   
   return admin.firestore()
     .collection('news')
