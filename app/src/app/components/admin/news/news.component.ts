9d504d3c061bf7a8dbaa27e096e1ef7e4f61f33c
alek98 <alek@spectra-alarm.com>
2021-04-07 09:35:58 +0200

    added table for news

--- app/src/app/components/admin/news/news.component.ts
+++ app/src/app/components/admin/news/news.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { News } from 'src/app/models/news.model';
 
 @Component({
   selector: 'app-news',
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewsComponent implements OnInit {
 
+  dummyNews: News = {
+    heading: 'New Destination: Mont-Saint-Michel.',
+    content: 'his complex exemplifies the outstanding universal values upheld by UNESCO.',
+    chName: 'Mont-Saint-Michel'
+  }
+  displayedColumns: string[] = ['heading', 'content', 'chName', 'edit', 'delete'];
+  
+  news$ = [this.dummyNews];
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  openEditDialog(news: News) {
+
+  }
+
+  openDeleteDialog(news: News) {
+
   }
 
 }
