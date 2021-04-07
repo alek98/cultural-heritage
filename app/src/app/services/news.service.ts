8c14b0e8a196edcd7b6f517188eecfc22ab4aab4
alek98 <alek@spectra-alarm.com>
2021-04-07 10:12:32 +0200

    news service created

--- app/src/app/services/news.service.ts
+++ app/src/app/services/news.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { AngularFireFunctions } from '@angular/fire/functions';
+import { News } from '../models/news.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NewsService {
 
-  constructor() { }
+  constructor(
+    private fns: AngularFireFunctions,
+    private firestore: AngularFirestore,
+  ) { }
+
+  addNews(news: News) {
+    const callable = this.fns.httpsCallable<News>('addNews');
+    return callable(news).toPromise();
+  }
 }
