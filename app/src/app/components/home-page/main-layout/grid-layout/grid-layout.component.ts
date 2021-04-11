b213ae201a1efb34b1d5a2f1f930b69c1a8c671d
alek98 <alek@spectra-alarm.com>
2021-04-11 17:06:28 +0200

    added responsiveness to the grid

--- app/src/app/components/home-page/main-layout/grid-layout/grid-layout.component.ts
+++ app/src/app/components/home-page/main-layout/grid-layout/grid-layout.component.ts
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./grid-layout.component.css']
 })
 export class GridLayoutComponent implements OnInit {
+  breakpoint: number;
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.breakpoint = (window.innerWidth <= 600) ? 2 : 4;
+  }
+
+  onResize(event) {
+    this.breakpoint = (event.target.innerWidth <= 600) ? 2 : 4;
   }
 
 }
